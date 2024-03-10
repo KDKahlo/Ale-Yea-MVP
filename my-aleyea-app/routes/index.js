65c5be5a2460bf8b550c0815242f7465e735c488
KDKahlo <132831985+KDKahlo@users.noreply.github.com>
2024-03-10 18:05:31 +0100

    routers for breweries by id and craftbeers by flavor

--- my-aleyea-app/routes/index.js
+++ my-aleyea-app/routes/index.js
@@ -17,7 +17,7 @@ router.get("/breweries", async (req, res) => {
     const query = "SELECT * FROM breweries ORDER BY id;";
     const results = await db(query);
     //console.log(`The results are: ${results}`);
-    console.log(`The results are: ${JSON.stringify(results)}`);
+  
 
     res.send(results.data);
     
@@ -26,15 +26,31 @@ router.get("/breweries", async (req, res) => {
     res.status(500).send(err);
   }
 });
-
+//get breweries by id
+router.get("/breweries/:id", async (req, res) => {
+  const {id} = req.params;
+  // Send back the full list of breweries
+  console.log('reached the endpoint')
+  try {
+    const query =`SELECT * FROM breweries WHERE id = ${id};`;
+    const results = await db(query);
+    //console.log(`The results are: ${results}`);
+    res.send(results.data);
+    
+  } catch (err) {
+    console.error("Error occurred:", err);
+    res.status(500).send(err);
+  }
+});
+//get full list of craftbeers
 router.get("/craftbeers", async (req, res) => {
   // Send back the full list of craftbeers
   console.log('reached the endpoint')
   try {
-    const query = "SELECT * FROM craftbeers ORDER BY id;";
+    const query = "SELECT * FROM craftbeers ORDER BY id ASC;";
     const results = await db(query);
     //console.log(`The results are: ${results}`);
-    console.log(`The results are: ${JSON.stringify(results)}`);
+   //
 
     res.send(results.data);
     
@@ -43,6 +59,21 @@ router.get("/craftbeers", async (req, res) => {
     res.status(500).send(err);
   }
 });
-
-
+//get back list of craftbeer according to the flavor
+//api in postman needs to be structured like this ---->  localhost:4000/craftbeers/your flavor choice here 
+router.get("/craftbeers/:flavor", async (req, res) => {
+  const {flavor} = req.params;
+  // Send back the full list of craftbeers
+  console.log('reached the endpoint')
+  try {
+    const query =`SELECT * FROM craftbeers WHERE flavor LIKE '%${flavor}%';`;
+    const results = await db(query);
+    //console.log(`The results are: ${results}`);
+    res.send(results.data);
+    
+  } catch (err) {
+    console.error("Error occurred:", err);
+    res.status(500).send(err);
+  }
+});
 module.exports = router;
