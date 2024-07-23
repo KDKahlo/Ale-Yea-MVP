d2fa5320f56189d38253c2954e92084242888e31
KD <kdkahlo@gmail.com>
2024-07-23 17:57:17 +0200

    juction table for breweries and craftbeer added to init_db.sql

--- my-aleyea-app/model/init_db.sql
+++ my-aleyea-app/model/init_db.sql
@@ -16,18 +16,23 @@ CREATE TABLE `breweries`(
     `breweryurl` VARCHAR(255) NOT NULL,
     `location` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `craftbeers`(
+CREATE TABLE `craftbeers` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `breweries-id` BIGINT NOT NULL,
+    `breweries_id` BIGINT NOT NULL,
     `beername` VARCHAR(255) NOT NULL,
     `flavor` VARCHAR(255) NOT NULL,
-    `type/style` VARCHAR(255) NOT NULL,
+    `type_style` VARCHAR(255) NOT NULL,
     `ABV` INT NOT NULL,
     `color` VARCHAR(255) NOT NULL,
-    `description` VARCHAR(255) NOT NULL
-    `guest_beer` tinyint(1)  NOT NULL,
-    `image_url` VARCHAR(255) NOT NULL
-
+    `description` VARCHAR(255) NOT NULL,
+    `guest_beer` TINYINT(1) NOT NULL,
+    `image_url` VARCHAR(400) NOT NULL
 );
--- ALTER TABLE
---     `craftbeers` ADD CONSTRAINT `craftbeers_breweries_id_foreign` FOREIGN KEY(`breweries-id`) REFERENCES `breweries`(`id`);
+-- junction table to allow 1 brewerery to be linked to many craft beers and 1 craft beer to many breweries.
+CREATE TABLE `brewery_craftbeer` (
+    `brewery_id` BIGINT UNSIGNED NOT NULL,
+    `craftbeer_id` BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`brewery_id`, `craftbeer_id`),
+    CONSTRAINT `fk_brewery_id` FOREIGN KEY (`brewery_id`) REFERENCES `breweries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `fk_craftbeer_id` FOREIGN KEY (`craftbeer_id`) REFERENCES `craftbeers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
