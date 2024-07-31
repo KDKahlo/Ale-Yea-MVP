f945461f3db844c1d067b9d4dacdf0f7776e535d
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2024-07-31 11:42:57 +0200

    NavBar CSS update
    - lots of refactoring
    - changes in project structure
    - added NavBar to App.jsx to make it globally available on each Page
    - added responsive design for smaller viewport
    
    ### additional
    - added HeroTeaser
    - moved Carousel into own file
    - kept css files for each component

--- client/src/components/AleYea.jsx
+++ client/src/components/AleYea.jsx
@@ -3,110 +3,71 @@ import { Link } from "react-router-dom";
 import "./AleYea.css";
 
 export default function AleYea({ fetchFlavorRecommendations, setError, flavor, error }) {
-  const [recommendations, setRecommendations] = useState([]);
-  const [inputValue, setInputValue] = useState(''); // Renamed flavor state variable to inputValue
+    const [recommendations, setRecommendations] = useState([]);
+    const [inputValue, setInputValue] = useState(""); // Renamed flavor state variable to inputValue
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetchFlavorRecommendations(inputValue);
+            console.log(response.data);
+            setRecommendations(response.data);
+        } catch (error) {
+            setError("No beers found with that flavor. Please try another flavor.");
+            console.error(error);
+        }
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetchFlavorRecommendations(inputValue);
-        console.log(response.data);
-      setRecommendations(response.data);
-    
-    } catch (error) {
-      setError('No beers found with that flavor. Please try another flavor.');
-      console.error(error);
-    }
-  };
-  
+    return (
+        <div className="AleYea">
+            <div className="container-flavor-choice-and-title">
+                <div className="header">
+                    <h1 className="archivo-black-regular">Ale Yea!</h1>
+                </div>
 
-  return (
-    <div className="AleYea">
-      {/* Navbar */}
-      <div className="navBar ">
-        <div className = " bg-warning-subtle text-warning-emphasis position-absolute row col-sm-12">
-        <nav className="navbar bg-body-tertiary p-3 mb-2 bg-warning-subtle text-warning-emphasis fixed-top">
-        <div className="container-fluid p-3 mb-2 bg-warning-subtle text-warning-emphasis">
-          <div className="appLogoImg" >
-            <img src="\images\Ale-Yea-Updated-Logo.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-           </div>
-            <ul className="nav p-3 mb-2 bg-warning-subtle text-warning-emphasis">
-                <li className="">
-                     <Link className="nav-link text-body-tertiary"  to="/">HOME</Link>
-                </li>
-                <li className="nav-item">
-                     <Link className="nav-link text-body-tertiary " to="/AleVenture">ALE-VENTURE!</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link text-body-tertiary" to="/AleYea">ALE-YEA!</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link text-body-tertiary"to="/CraftbeerMe">CRAFTBEER ME!</Link>
-                </li>
-            </ul>
+                <div className="flavor-form">
+                    <form onSubmit={handleSubmit}>
+                        <div className="" id="label-input-button">
+                            <label htmlFor="exampleDataList" className="form-label protest-strike-regular">
+                                ALE YEA, I know which flavor I want!{" "}
+                            </label>
+                            <div className="" id="input-group">
+                                <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type a flavor..." type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                                <button className="btn mb-3 protest-strike-regular" type="submit">
+                                    Tap 🍺 to Submit
+                                </button>
+                            </div>
+                        </div>
+                        <datalist id="datalistOptions">
+                            <option value="chocolate" />
+                            <option value="fruity" />
+                            <option value="sour" />
+                            <option value="caramel" />
+                            <option value="tropical" />
+                        </datalist>
+
+                        <div ClassName="col-auto"></div>
+                    </form>
+                    {error && <p>{error}</p>}
+                </div>
+            </div>
+
+            <div className="container">
+                <main className="grid">
+                    {recommendations.map((beer) => (
+                        <article>
+                            <div key={beer.id}>
+                                <img src={beer.image_url} alt="image of beer recommendation" />
+
+                                <div className="text">
+                                    <h3>{beer.beername}</h3>
+                                    <p>{beer.description}</p>
+                                </div>
+                            </div>
+                        </article>
+                    ))}
+                </main>
+            </div>
         </div>
-      </nav> 
-       </div>
-        </div>
-<div className=  "container-flavor-choice-and-title">
-      <div className = "header">
-      <h1 className="archivo-black-regular">Ale Yea!</h1>
-      </div>
-
-  <div className = "flavor-form">
-     <form onSubmit={handleSubmit}>
-  
-
-    <div className = "" id = "label-input-button">
-          <label htmlFor="exampleDataList" className="form-label protest-strike-regular">ALE YEA, I know which flavor I want! </label>
-        <div className = "" id = "input-group"> 
-             <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type a flavor..." type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-             <button className="btn mb-3 protest-strike-regular" type="submit">Tap 🍺 to Submit</button>
-    </div>
-
-        </div>
-          <datalist id="datalistOptions">
-              <option value="chocolate"/>
-              <option value="fruity"/>
-              <option value="sour"/>
-              <option value="caramel"/>
-              <option value="tropical"/>
-          </datalist>
-       
-          <div ClassName = "col-auto">
-         
-         </div>
-     </form>
-         {error && <p>{error}</p>}
-  </div>
-</div>
-
- <div className= "container">  
-  <main className = "grid">
-          {recommendations.map((beer) => (
-    <article>
-     <div key={beer.id} >
-       
-       <img src={beer.image_url}
-      alt="image of beer recommendation"/>
-
-        <div className="text">
-           <h3>{beer.beername}</h3>
-           <p>{beer.description}</p>
-         
-        </div>
-          
-       
-     </div> 
-     </article>
-      ))}
-     
-      </main>
- </div>
-   
-       
-      </div>
-  
-  );
+    );
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,103 +1,17 @@
 import React from "react";
 import "./Home.css";
-import { Routes, Route, Link} from "react-router-dom"
-
-
+import HeroTeaser from "./HeroTeaser";
+import Carousel from "./Carousel";
+import { Routes, Route, Link } from "react-router-dom";
 
 function Home() {
     return (
-
-     
-        
         <div className="background-container mt-0">
-          <div className = "headerSection">
-        {/* <img   className = "headerSection z-0 position-absolute" src="\images\Ale-Yea-Background-right.png"/>  */}
-          </div>
-        {/* Navbar */}
-        <div className="navBar ">
-        <div className = " bg-warning-subtle text-warning-emphasis position-absolute row col-sm-12">
-        <nav className="navbar bg-body-tertiary p-3 mb-2 bg-warning-subtle text-warning-emphasis fixed-top">
-        <div className="container-fluid p-3 mb-2 bg-warning-subtle text-warning-emphasis">
-          <div className="appLogoImg" >
-            <img src="\images\Ale-Yea-Updated-Logo.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-           </div>
-            <ul className="nav p-3 mb-2 bg-warning-subtle text-warning-emphasis">
-                <li className="">
-                     <Link className="nav-link text-body-tertiary"  to="/">HOME</Link>
-                </li>
-                <li className="nav-item">
-                     <Link className="nav-link text-body-tertiary" to="/AleVenture">ALE-VENTURE!</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link text-body-tertiary" to="/AleYea">ALE-YEA!</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link text-body-tertiary"to="/CraftbeerMe">CRAFTBEER ME!</Link>
-                </li>
-            </ul>
+            <main>
+              <HeroTeaser />
+              <Carousel />
+            </main>
         </div>
-      </nav> 
-       </div>
-        </div>
-       
-
-
-    <div className="carouselImages col-md-8 protest-strike-regular ">
-        <div id="carouselExampleAutoplaying" className="carousel slide position-absolute top-50 start-0 translate-middle-y ">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div className="carousel-inner" >
-          <div className="carousel-item active" data-bs-interval="100">
-            <img src="images/photo-1615332579037-3c44b3660b53.avif" className="d-block  w-100"   alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>LET THE ALE-VENTURE BEGIN!</h5>
-              <p> Craft beer isn't just a beverage. It's a masterpiece in a pint glass!
-                Get ready to sip on the liquid artistry that is craft beer!
-                Before we start this ale-tastic journey, lets look at what makes this experience so unique.</p>
-                <Link to="/AleVenture" className="text-white-50">HOP ON OVER TO ALE-VENTURE!</Link>
-            </div>
-          </div>
-          <div className="carousel-item" data-bs-interval="100">
-            <img src="images/photo-1505075106905-fb052892c116.avif" className="d-block w-100"  alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>CRAFTBEER ME!</h5> 
-              <p>Alright, time to pick your flavor adventure! Are you feeling sweet like caramel, 
-                fruity like a tropical vacation, nutty like your quirky neighbor,
-                 or perhaps creamy like a milkshake that brings all the folks to the yard? 
-                Then dive into the flavorful world of craft beer with
-                our custom quiz and find your the perfect match.</p>
-                <Link to="/CraftbeerMe "  className="text-white-50">HOP ON OVER TO CRAFTBEER ME!</Link>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src="\images\photo-1584225064785-c62a8b43d148.avif" className="d-block  w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>ALE-YEA I KNOW MY BREW!</h5>
-              <p>Craft Beer Connoisseurs, take the express lane to your brew haven! With a swift search,
-                 dive into your favorite flavors or scout local sips nearby.
-                 No more wild goose chases—just hoppy adventures await with a quick tap on ALE-YEA. 🍻</p>
-                 <Link to="/AleYea"  className="text-white-50">HOP ON OVER TO ALE-YEA!</Link>                 
-            </div>
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-       </div>
-     
-      </div>
-     
-    
-</div> 
-     
     );
 }
 
