b7ce69220298655dc92f358b1874032b4f823aed
KD <kdkahlo@gmail.com>
2024-08-01 08:27:04 +0200

    modification to fetchFlavorRecommendations function in App.jsx to include breweries info for beer results

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,10 +18,29 @@ export default function App() {
       const response = await fetch(`/api/craftbeers/${flavor}`);
       console.log('this is my fetch response for fetchFlavorRecommendations:', response);
 
-      const data = await response.json();
-      console.log(data)
-      return data;
-      
+      const craftbeerData = await response.json();
+      console.log( craftbeerData)
+       // Extract unique breweries_id values from the craftbeerData
+      const breweryIds = [...new Set(craftbeerData.map(item => item.breweries_id))];
+
+         // Fetch brewery information for each unique brewery_id
+         let breweryPromises = breweryIds.map(id => fetch(`/api/breweries/${id}`).then(res => res.json()));
+         let breweriesData = await Promise.all(breweryPromises);
+
+          // Create a map of brewery_id to brewery data for easy lookup
+        let breweryMap = {};
+        breweriesData.forEach(brewery => {
+            breweryMap[breweries.id] = brewery;
+          });
+
+          // Add brewery information to each craftbeer item
+          let craftbeerResult = craftbeerData.map(craftbeer => ({
+              ...craftbeer,
+              brewery: breweryMap[craftbeer.brewery_id]
+          }));
+
+      return  craftbeerResult;
+
     } catch (error) {
    
       throw new Error('Failed to fetch beer recommendations. Please try again.');
